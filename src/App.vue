faae240a2543b4748a67f60af704a1af8e194ab0
trastanechora <trastanechora@gmail.com>
2021-12-16 09:56:22 +0800

    Add Vue CLI demo full version

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-	data() {
-    return {
-      pesan: 'Halo Vue! '
-    }
-  },
-  computed: {
-    pesanGanda() {
-      return this.pesan + this.pesan;
-    }
-  },
-  methods: {
-    munculkanAlert() {
-      alert(this.pesan);
-    }
-  },
-  created() {
-    console.log(this.pesanGanda);
-  },
-  mounted() {
-    this.munculkanAlert();
-  },
-  watch: {
-    pesan(nilaiBaru, nilaiLama) {
-      console.log(`Pesan telah berubah dari ${nilaiLama} menjadi ${nilaiBaru}`);
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -49,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
