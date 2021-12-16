59a82cd40e6233e26d9d3ae944cb3b61580af8c6
trastanechora <trastanechora@gmail.com>
2021-12-16 09:28:33 +0800

    Add Vue CLI demo files

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+	data() {
+    return {
+      pesan: 'Halo Vue! '
+    }
+  },
+  computed: {
+    pesanGanda() {
+      return this.pesan + this.pesan;
+    }
+  },
+  methods: {
+    munculkanAlert() {
+      alert(this.pesan);
+    }
+  },
+  created() {
+    console.log(this.pesanGanda);
+  },
+  mounted() {
+    this.munculkanAlert();
+  },
+  watch: {
+    pesan(nilaiBaru, nilaiLama) {
+      console.log(`Pesan telah berubah dari ${nilaiLama} menjadi ${nilaiBaru}`);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
