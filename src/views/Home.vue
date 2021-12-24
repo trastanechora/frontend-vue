d48246ef209ba2cad4b26d089051e050234bc100
trastanechora <trastanechora@gmail.com>
2021-12-24 17:29:32 +0800

    Add vuex store demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>Angka dari store: {{ angkaDariStore }}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  computed: {
+    angkaDariStore() {
+      return this.$store.state.angka;
+    },
   },
 };
 </script>
