799d397d2bebf594853aded22dd9d3e7072e69ad
trastanechora <trastanechora@gmail.com>
2021-12-25 02:22:11 +0800

    Add vuex actions mutations

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,24 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     angka: 14,
+    infoText: "Angka Lebih besar dari sepuluh",
   },
-  mutations: {},
-  actions: {},
-  modules: {},
+  mutations: {
+    setAngka(state, param) {
+      state.angka = param;
+    },
+    setInfo(state, param) {
+      state.infoText = param;
+    },
+  },
+  actions: {
+    changeAngkaValue(store, param) {
+      store.commit("setAngka", param);
+      if (param > 10) {
+        store.commit("setInfo", "Angka Lebih besar dari sepuluh");
+      } else {
+        store.commit("setInfo", "Angka Lebih kecil dari sepuluh");
+      }
+    },
+  },
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <p>Angka dari store: {{ angkaDariStore }}</p>
+    <p>{{ infoDariStore }}</p>
+    <input type="number" v-model.number="newInputValue" />
+    <button @click="changeAngka">Ganti Angka di Store</button>
   </div>
 </template>
 
@@ -10,6 +13,19 @@ export default {
   computed: {
     angkaDariStore() {
       return this.$store.state.angka;
+    },
+    infoDariStore() {
+      return this.$store.state.infoText;
+    },
+  },
+  data() {
+    return {
+      newInputValue: 0,
+    };
+  },
+  methods: {
+    changeAngka() {
+      this.$store.dispatch("changeAngkaValue", this.newInputValue);
     },
   },
 };
