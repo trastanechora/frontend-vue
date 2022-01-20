d7e83877ac79129a692d2c5c352a8657492bf1c9
trastanechora <maestro.trastanechora@tokopedia.com>
2022-01-21 04:11:21 +0800

    Add login page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <!-- <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -31,7 +31,7 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar> -->
 
     <v-main>
       <router-view />
---
+++ src/views/Login.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-layout wrap class="mx-4">
+    <v-flex xs12>
+      <v-img
+        :src="require('../assets/illustration.png')"
+        max-width="250"
+        class="ma-auto mt-10"
+      />
+    </v-flex>
+    <v-flex xs12>
+      <v-form ref="loginForm">
+        <v-text-field
+          outlined
+          label="Email"
+          v-model="email"
+          :disabled="isLoading"
+          :loading="isLoading"
+        ></v-text-field>
+        <v-text-field
+          outlined
+          label="Password"
+          :type="showPassword ? 'text' : 'password'"
+          v-model="password"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+          :disabled="isLoading"
+          :loading="isLoading"
+        ></v-text-field>
+        <v-btn
+          block
+          color="primary"
+          @click="doLogin"
+          :disabled="isLoading"
+          :loading="isLoading"
+          >Login</v-btn
+        >
+        <p class="registerText">
+          New to Sally? <a @click="doRegister">Sign up</a>
+        </p>
+      </v-form>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+      showPassword: false,
+      isLoading: false,
+    };
+  },
+  methods: {
+    doLogin() {
+      console.warn("Melakukan Login!");
+      this.isLoading = true;
+    },
+    doRegister() {
+      console.warn("Melakukan Register");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.v-btn {
+  text-transform: none;
+  letter-spacing: normal;
+}
+.registerText {
+  text-align: center;
+  margin-top: 8px;
+}
+</style>
